c932a5b5b9d2ee428045b2233da00d91b65b6507
JuanMelendres <juan.mele97@gmail.com>
2020-02-21 11:17:19 -0600

    Add Class 02 folder
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- FirstPartial/Class/Class-2/app/src/main/java/com/itesm/localstorage/DBHelper.java
+++ FirstPartial/Class/Class-2/app/src/main/java/com/itesm/localstorage/DBHelper.java
@@ -1,4 +1,49 @@
 package com.itesm.localstorage;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
+
+/*
+    1era estrategia - utilizar una base de datos local
+    sqlite ?
+    manejador de base de datos relacionales
+    se guarda en un archivo local
+    como es relacional utiliza queries
+
+*/
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_FILE = "Database.db";
+    private static final String TABLE = "Estudiantes";
+    private static final String FIELD_ID = "id";
+    private static final String FIELD_NAME = "nombre";
+    private static final String FIELD_GRADE = "calificacion";
+
+    public DBHelper(Context context) {
+        super(context, DB_FILE, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE + "(" +
+                FIELD_ID+"INTEGER PRIMARY KEY, " +
+                FIELD_NAME+"TEXT, " +
+                FIELD_GRADE+ "INTEGER)";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // si cambiamos de version luego
+        // prepared statements
+        String query = "DROP TABLE IF EXISTS ?";
+        String[] params = {TABLE};
+        db.execSQL(query, params);
+    }
 }
---
+++ FirstPartial/Class/Class-2/app/src/main/java/com/itesm/localstorage/MainActivity.java
@@ -0,0 +1,17 @@
+package com.itesm.localstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Usando BD local en sqlite
+
+    }
+}
