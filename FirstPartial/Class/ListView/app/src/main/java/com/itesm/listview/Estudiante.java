6e1998120793fdaac6fcfc2e3b25c245214d8a14
JuanMelendres <juan.mele97@gmail.com>
2020-03-04 11:01:14 -0600

    Add list view example
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- FirstPartial/Class/ListView/app/src/main/java/com/itesm/listview/Estudiante.java
+++ FirstPartial/Class/ListView/app/src/main/java/com/itesm/listview/Estudiante.java
@@ -1,4 +1,28 @@
 package com.itesm.listview;
 
+// bean: Clase que solo contiene datos
+
 public class Estudiante {
+
+    private String nombre;
+    private float calificacion;
+    private long studentId;
+
+    public Estudiante(String nombre, float calificacion, long studentId) {
+        this.nombre = nombre;
+        this.calificacion = calificacion;
+        this.studentId = studentId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getCalificacion() {
+        return calificacion;
+    }
+
+    public long getStudentId() {
+        return studentId;
+    }
 }
--- FirstPartial/Class/ListView/app/src/main/java/com/itesm/listview/MainActivity.java
+++ FirstPartial/Class/ListView/app/src/main/java/com/itesm/listview/MainActivity.java
@@ -3,12 +3,37 @@ package com.itesm.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // datos
+        String[] datos = {"Estudiante 1", "Estudiante 2", "Estudiante 3"};
+        // para conectar estos dos existe un adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+            this,
+                android.R.layout.simple_list_item_1,
+                datos
+        );
+
+        ArrayList<Estudiante> estudiantitos = new ArrayList<>();
+        estudiantitos.add(new Estudiante("Rafa", 25, 12));
+        estudiantitos.add(new Estudiante("Juan Daniel", 24, 14));
+        estudiantitos.add(new Estudiante("Gus", 24, 15));
+
+        EstudianteAdapter adatador = new EstudianteAdapter(estudiantitos, this);
+
+        // vista
+        listita = findViewById(R.id.listita);
+        listita.setAdapter(adatador);
     }
 }
