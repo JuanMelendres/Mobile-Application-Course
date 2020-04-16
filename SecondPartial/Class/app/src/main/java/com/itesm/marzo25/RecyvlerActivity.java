3330f826a9325dd3cc1a377723f505a08647caa8
JuanMelendres <juan.mele97@gmail.com>
2020-04-16 11:52:09 -0500

    Add class work 1
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ SecondPartial/Class/app/src/main/java/com/itesm/marzo25/RecyvlerActivity.java
@@ -0,0 +1,13 @@
+package com.itesm.marzo25;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecyvlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyvler);
+    }
+}
---
+++ SecondPartial/Class/app/src/main/java/com/itesm/marzo25/StudentAdapter.java
@@ -0,0 +1,4 @@
+package com.itesm.marzo25;
+
+public class StudentAdapter {
+}
